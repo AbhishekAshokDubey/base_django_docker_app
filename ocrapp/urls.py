b177be18a24a7cb9b2e47bcf6079dd216e3675ed
Abhishek Dubey <adubey4@slb.com>
2019-08-01 01:22:14 +0530

    pull version

--- ocrapp/urls.py
+++ ocrapp/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     url(r'^$', views.home, name='home'),
     url(r'^gettext$', views.get_text, name='get_text'),
+    url(r'^uploadresult$', views.upload_result, name='upload_result'),
 ]
--- pull_script/pull_pdfs_ocr.py
+++ pull_script/pull_pdfs_ocr.py
@@ -1,24 +1,18 @@
-from django import http
-from django.core import serializers
-#from django.core.urlresolvers import reverse
-#from django.db import connection, transaction
-#from django.db.transaction import Transaction
-from django.http import Http404
-from django.http import HttpResponse
-from django.http import HttpResponseRedirect, HttpResponse
-from django.shortcuts import render, redirect, render_to_response
-from django.template import loader
-from django.views import generic
-import json
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug  1 00:33:20 2019
 
-# Create your views here.
+@author: ADubey4
+"""
+
 import os
 import sys
 import pytesseract 
 from PIL import Image 
 from sys import platform
 from pdf2image import convert_from_path 
-
+import subprocess
+import time
 
 if platform == "linux" or platform == "linux2":
     base_data_folder = "/base_django_docker_app/data"
@@ -26,27 +20,25 @@ elif platform == "win32":
     pytesseract.pytesseract.tesseract_cmd = 'C:/Program Files (x86)/Tesseract-OCR/tesseract'
     base_data_folder = r"C:\Users\Adubey4\Desktop\test\base_django_docker_app\data"
 
-#elif platform == "darwin":
-    # OS X
-
-def home(request):
-	return HttpResponse("Hello World")
 
 def copy_file_from_bucket(PDF_file_path_gcp):
     cmd = "gsutil cp "+ PDF_file_path_gcp +" \""+ base_data_folder+"\""
-    print(cmd)
+#    print(cmd)
     os.system(cmd)
 
 def upload_file_to_bucket(local_path, bucket_path):
     cmd = "gsutil cp "+ local_path +" \""+ bucket_path +"\""
-    print(cmd)
+#    print(cmd)
     os.system(cmd)
     try:
         os.remove(local_path)
         os.remove(local_path.replace(".txt",".pdf"))
     except:
         print("count not delete files")
-    
+
+def remove_from_bucket(file_path):
+    os.system("gsutil rm "+file_path)
+
 def save_ocr_text(PDF_file_path_gcp):
     pdf_name = os.path.basename(PDF_file_path_gcp)
     PDF_file_path = os.path.join(base_data_folder, pdf_name)
@@ -66,19 +58,17 @@ def save_ocr_text(PDF_file_path_gcp):
     f.close()
     return out_file_path
 
-def get_text(request):
-    PDF_file_path_gcp = request.GET.get("path")
-    copy_file_from_bucket(PDF_file_path_gcp)
-    out_file_path = save_ocr_text(PDF_file_path_gcp)
-    with open(out_file_path, 'r') as file:
-        data = file.read()
-    #http://localhost:8000/gettext?path=gs://abhishek-test/input/sample.pdf
-    return HttpResponse(json.dumps(data))
 
-def upload_result(request):
-    PDF_file_path_gcp = request.GET.get("path")
-    copy_file_from_bucket(PDF_file_path_gcp)
-    out_file_path = save_ocr_text(PDF_file_path_gcp)
-    upload_file_to_bucket(out_file_path, PDF_file_path_gcp.replace("/input/","/output/").replace(".pdf",".txt"))
-    #http://localhost:8000/uploadresult?path=gs://abhishek-test/input/sample.pdf
-    return HttpResponse("Uploaded")
+if __name__ == "__main__":
+    while True:
+        result = subprocess.run(['gsutil', 'ls', "gs://abhishek-test/input/*.pdf"], stdout=subprocess.PIPE, shell=True)
+        pdf_files = result.stdout.strip().decode("utf-8").replace("\r","").split("\n")
+        for PDF_file_path_gcp in pdf_files:
+            try:
+                copy_file_from_bucket(PDF_file_path_gcp)
+                out_file_path = save_ocr_text(PDF_file_path_gcp)
+                upload_file_to_bucket(out_file_path, PDF_file_path_gcp.replace("/input/","/output/").replace(".pdf",".txt"))
+                remove_from_bucket(PDF_file_path_gcp)
+            except:
+                print("Broke for: "+PDF_file_path_gcp)
+        time.sleep(10) # 2 second delay
